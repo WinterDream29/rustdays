8314cd10149adb45e2b5cc1037adf1391d5b4824
Bublik <alukardev@gmail.com>
2019-02-06 08:40:45 +0300

    edit map

---
+++ SoporNew/Assets/Scripts/Map/CameraBounds.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Map
+{
+    [RequireComponent(typeof(BoxCollider))]
+    public class CameraBounds : MonoBehaviour
+    {
+        public Camera LinkedCamera;
+
+        private BoxCollider _boxCollider;
+
+        private void Start()
+        {
+            _boxCollider = this.GetComponent<BoxCollider>();
+        }
+
+        private void LateUpdate()
+        {
+            float vertExtent = LinkedCamera.orthographicSize;
+            float horizExtent = vertExtent * Screen.width / Screen.height;
+
+            Vector3 linkedCameraPos = LinkedCamera.transform.position;
+            Bounds areaBounds = _boxCollider.bounds;
+
+            //LinkedCamera.transform.position = new Vector3(
+            //    Mathf.Clamp(linkedCameraPos.x, areaBounds.min.x + horizExtent, areaBounds.max.x - horizExtent),
+            //    Mathf.Clamp(linkedCameraPos.y, areaBounds.min.y + vertExtent, areaBounds.max.y - vertExtent),
+            //    linkedCameraPos.z);
+
+            LinkedCamera.transform.position = new Vector3(
+                Mathf.Clamp(linkedCameraPos.x, areaBounds.min.x + horizExtent, areaBounds.max.x - horizExtent),
+                linkedCameraPos.y,
+                Mathf.Clamp(linkedCameraPos.z, areaBounds.min.z + vertExtent, areaBounds.max.z - vertExtent));
+        }
+    }
+}
---
+++ SoporNew/Assets/Scripts/Map/DragCamera.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Map
+{
+    public class DragCamera : MonoBehaviour
+    {
+        public float DragSpeed = 2;
+        public float OuterLeft = -10f;
+        public float OuterRight = 10f;
+        public float OuterTop = 10f;
+        public float OuterDown = 10f;
+
+        private Vector3 _dragOrigin;
+        public bool CameraDragging = true;
+
+        void Update ()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                _dragOrigin = Input.mousePosition;
+                return;
+            }
+
+            if (!Input.GetMouseButton(0)) return;
+
+            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - _dragOrigin);
+            Vector3 move = new Vector3(pos.x * DragSpeed, 0, pos.y * DragSpeed);
+
+            transform.Translate(move, Space.World);
+        }
+    }
+}
